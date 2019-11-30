93c20a9cdb03e60e468f03ba8df21986a57bc090
hejianxin <hejianxinsir@gmail.com>
2019-11-30 10:37:11 +0800

    change content

--- src/App.js
+++ src/App.js
@@ -5,17 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    		<div>Hello, this is my first React app. Let's read a poem, shall we ?</div>
-				<div className="poem">
-					<div>小池</div>
-					<div>宋-杨万里</div>
-					<div>
-						泉眼无声惜细流，<br />
-						树阴照水爱晴柔。<br />
-						小荷才露尖尖角，<br />
-						早有蜻蜓立上头。<br />
-					</div>
-				</div>
+			<p>Hello, this is my first React App.</p>
     </div>
   );
 }
