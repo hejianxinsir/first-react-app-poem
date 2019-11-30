ba36d85919b95535f060d04d10228c33a9adf7ad
hejianxin <hejianxinsir@gmail.com>
2019-11-30 10:54:27 +0800

    add some style to poem

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-			<p>Hello, this is my first React App.</p>
+			<p class="welcome">
+			Hello, this is my first React App.<br/>
+			Well, nothing else, but read a poem, shall we ? 
+			</p>
+			<div className="poem">
+				<h3>小池</h3>
+				<footer>宋 杨万里</footer>
+				<p>
+					泉眼无声惜细流，<br/>
+					树阴照水爱晴柔。<br/>
+					小荷才露尖尖角，<br/>
+					早有蜻蜓立上头。<br/>
+				</p>
+			</div>
     </div>
   );
 }
